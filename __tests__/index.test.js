12ff632295e825a63e8744216a3920fc491cb923
Alina Abdul <alinka140901@gmail.com>
2022-08-03 17:28:13 +0300

    added workflow

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,24 @@
 import reverse from '../src/index.js';
+import _ from 'lodash';
 
 test('reverse', () => {
     expect(reverse('Hello')).toEqual('olleH');
     expect(reverse('')).toEqual('');
 });
+
+const collection = ['one', true, 3, 4, 'cat', {},''];
+
+test('includes', () => {
+    expect(_.includes(collection,3)).toBe(true);
+    expect(_.includes(collection, 11)).toBe(false);
+});
+
+test('filter', () => {
+    expect(_.filter(collection,(v) => v % 2 === 0)).toEqual([4, '']);
+});
+
+let now;
+
+beforeEach(() => {
+    now = Date.now();
+})
